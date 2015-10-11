fe3b4cedaf4d7a28081e5394158a1331896b3fbc
Shannon Rhodes <srhodes-phd@users.noreply.github.com>
2015-10-11 16:08:45 -0700

    Final last little changes

--- plot4.R
+++ plot4.R
@@ -40,7 +40,7 @@ with(sub, {
       with(sub, points(TimeCh, Sub_metering_2, type="l", col= "red"))
       with(sub, points(TimeCh, Sub_metering_3, type="l", col= "blue"))
       legend("topright", pch=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ))
-      plot(TimeCh,Global_reactive_power,type = "l", xlab = "dataetime", ylab = "Global Reactive Power")
+      plot(TimeCh,Global_reactive_power,type = "l", xlab = "datetime", ylab = "Global Reactive Power")
 })
 dev.off()
 
