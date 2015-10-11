c77ce1d86d86f7db0063ca562190d9378170c637
Shannon Rhodes <srhodes-phd@users.noreply.github.com>
2015-10-11 15:39:51 -0700

    Plot 3

---
+++ plot3.R
@@ -0,0 +1,54 @@
+# Exploratory Data Analysis
+# Project 1
+
+require(data.table)
+
+
+# create a folder for this course if it does not exist already 
+if (!file.exists("DS4")) {
+      dir.create("DS4")
+}
+
+# Download and unzip, the zipped file if the txt file  is not already in the folder
+# Must use mode="wb" because the zipped file is in binary
+
+if (!file.exists("./DS4/household_power_consumption.txt")) {
+      fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+      download.file(fileURL, destfile = "./DS4/power.zip", mode="wb")
+      unzip("./DS4/power.zip", exdir="./DS4")
+}
+
+# Read in the dataset
+header <- read.table("household_power_consumption.txt", sep=";", nrows=1)
+power <- read.table("household_power_consumption.txt", sep=";", na.strings = c("?"), skip=1)
+colnames(power) <- unlist(header)
+
+# Create the subset of data for the plots
+sub <- subset(power, Date == "2/1/2007" | Date == "2/2/2007")
+
+# Format the date and time fields
+sub$Date <- as.Date(sub$Date, format = "%m/%d/%Y") 
+timetemp <- paste(sub$Date, sub$Time)
+sub$TimeCh <- strptime(timetemp, format = "%Y-%m-%d %H:%M:%S") 
+
+
+png(file = "./ExData_Plotting1/plot3.png")
+
+with(sub, plot(TimeCh, Sub_metering_1, type = "n", ylab = "Energy Sub Metering", xlab = ""))
+with(sub, points(TimeCh, Sub_metering_1, type="l", col= "black"))
+with(sub, points(TimeCh, Sub_metering_2, type="l", col= "red"))
+with(sub, points(TimeCh, Sub_metering_3, type="l", col= "blue"))
+legend("topright", pch=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ))
+
+dev.off()
+
+
+
+
+
+
+
+
+
+
+# Data cleaning
